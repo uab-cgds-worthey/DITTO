abde5561fbca3081b2caabe5945629fbb6eedd05
Manavalan Gajapathy <manag@uab.edu>
2021-01-21 16:18:21 -0600

    adds cluster config

--- variant_annotation/run.sh
+++ variant_annotation/run.sh
@@ -11,3 +11,18 @@ module load snakemake/5.9.1-foss-2018b-Python-3.6.6
 
 # snakemake --use-conda -j 4 -k -p -f
 snakemake -s b37.Snakefile --use-conda -j 4 -k -p -f
+
+
+
+module reset
+module load Anaconda3/2020.02
+module load snakemake/5.9.1-foss-2018b-Python-3.6.6
+
+snakemake \
+	--snakefile "Snakefile" \
+	--use-conda \
+	--cluster-config '/data/project/worthey_lab/projects/experimental_pipelines/mana/small_var_caller_pipelines/dna-seq-gatk-variant-calling/adapt_to_CGDS_setup/configs/cluster_config.json' \
+	--cluster 'sbatch --ntasks {cluster.ntasks} --partition {cluster.partition} --cpus-per-task {cluster.cpus-per-task} --mem {cluster.mem} --output {cluster.output} --error {cluster.error} --parsable' \
+
+
+	--profile '/data/project/worthey_lab/projects/experimental_pipelines/mana/small_var_caller_pipelines/dna-seq-gatk-variant-calling/adapt_to_CGDS_setup/configs/snakemake_profile/{{cookiecutter.profile_name}}' \
