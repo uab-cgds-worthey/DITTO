7a0fc03b8edb5931896195e76f356c7064bee3a4
Tarun Karthik Kumar Mamidi <tmamidi@uab.edu>
2021-01-21 21:03:49 -0600

    download and parse/filter clinvar data

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # DITTO
 
 Diagnosis prediction tool using AI.
+
+Install packages and dependencies
+-------------------------------------
+srun --ntasks=1 --cpus-per-task=1 --mem-per-cpu=200G --time=01:59:00 --partition=pascalnodes --job-name=classify --pty /bin/bash
+Using conda envi to install and work on tools
+    module load Anaconda3/2020.02
+    conda env create --name training --file config/environment.yaml 
+
+
---
+++ src/clinvar/data-prep/filter.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+from tqdm import tqdm 
+import yaml
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+#import pickle
+
+def get_col_configs(config_f):
+    with open(config_f) as fh:
+        config_dict = yaml.safe_load(fh)
+
+    # print(config_dict)
+    return config_dict
+
+def extract_col(config_dict,df):
+    print('Extracting columns and rows according to config file !....')
+    df = df[config_dict['columns']]
+    df= df.loc[df['CLNSIG'].isin(config_dict['ClinicalSignificance']['6-class'])]
+    print('Dropping empty columns and rows...')
+    df.replace('.', np.nan, inplace=True)
+    df.dropna(axis=1, how='all', inplace=True)
+    df.dropna(axis=0, how='all', inplace=True)
+    print('CLNREVSTAT:', df['CLNREVSTAT'].nunique())
+    print('CLNVC:', df['CLNVC'].nunique())
+    print('MC:', df['MC'].nunique())
+    # CLNREVSTAT, CLNVC, MC
+    return df
+
+def fill_na(df): #(config_dict,df):
+    #df1  = pd.DataFrame()
+    var = df['AAChange.refGene']
+    df = df.drop('AAChange.refGene', axis=1)
+    df.to_csv('./data/interim/temp.csv', index=False)
+    df = pd.read_csv('./data/interim/temp.csv')
+    os.remove('./data/interim/temp.csv')
+    df = pd.get_dummies(df, prefix_sep='_')
+    df.columns.to_csv('./data/processed/clinvar_columns.csv', index=False)
+    #lr = LinearRegression()
+    #imp= IterativeImputer(estimator=lr, verbose=2, max_iter=10, tol=1e-10, imputation_order='roman')
+    print('Filling NAs ....')
+    #df = imp.fit_transform(df)
+    #filehandler = open('./data/processed/imputer.pkl', 'wb') 
+    #pickle.dump(imp, filehandler)
+    #df = pd.DataFrame(df, columns = columns)
+    df=df.fillna(df.median())
+    df['ID'] = [f'var_{num}' for num in range(len(df))]
+    #for key in tqdm(config_dict['Fill_NAs']):
+    #    if key in df.columns:
+    #        df1[key] = df[key].fillna(config_dict['Fill_NAs'][key]).astype('float64')
+    #    else:
+    #        df1[key] = config_dict['Fill_NAs'][key]
+    print('NAs filled!')
+    df = pd.concat([var.reset_index(drop=True), df], axis=1)
+    return df
+
+def main(var_f, config_f):
+    # read QA config file
+    config_dict = get_col_configs(config_f)
+    print('Config file loaded!\nNow loading data.....\n')
+    # read clinvar data
+    df = pd.read_csv(var_f)
+    print('Data Loaded !....')
+    df = extract_col(config_dict,df)
+    print('Columns extracted !....')
+    df.isnull().sum(axis = 0).to_csv('./data/processed/NA-count-6-class.csv')
+    y = df.CLNSIG.str.replace(r'/Likely_pathogenic','').str.replace(r'/Likely_benign','')
+    y = y.str.replace(r'Likely_benign','Benign').str.replace(r'Likely_pathogenic','Pathogenic')
+    df = df.drop('CLNSIG', axis=1)
+    df = fill_na(df) #(config_dict,df)
+    #print dataframe shape
+    #df.dtypes.to_csv('../../data/interim/head.csv')
+    print('Data shape=', df.shape)
+    print('Class shape=', y.shape)
+    df.to_csv('./data/processed/clinvar-md.csv', index=False)
+    y.to_csv('./data/processed/clinvar-y-md.csv', index=False)
+    return None
+
+if __name__ == "__main__":
+    os.chdir( '/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/')
+    var_f = "./data/external/clinvar.out.hg19_multianno.csv"
+    config_f = "./configs/col_config.yaml"
+    
+    main(var_f, config_f)
