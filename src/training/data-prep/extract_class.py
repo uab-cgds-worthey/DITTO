8f28e05fb934ee4315531212bcb6c28ac881a5d7
Tarun Karthik Kumar Mamidi <tmamidi@uab.edu>
2021-02-21 23:30:05 -0600

    filter variants after normalizing them

--- src/training/data-prep/extract_class.py
+++ src/training/data-prep/extract_class.py
@@ -1,8 +1,8 @@
 import os
 import gzip
 
-os.chdir( '/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/HGMD/')
-vcf = 'hgmd_pro_2020.4_hg38_edited_vep-annotated.vcf.gz'
+os.chdir( '/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
+vcf = '../interim/merged_sig_norm.vcf.gz'
 
 print("Collecting variant class...")
 class_dict = dict()
@@ -12,21 +12,22 @@ with gzip.open(vcf, 'rt') as vcffp:
                     line = line.rstrip("\n")
                     cols = line.split("\t")
                     var_info = cols[0]+"\t"+cols[1]+"\t"+cols[3]+"\t"+cols[4]
-                    hgmd_class = cols[7].split(";")[0].split("=")[1]
-                    class_dict[var_info] = hgmd_class
+                    #hgmd_class = cols[7].split(";")[0].split("=")[1]
+                    class_dict[var_info] = cols[5]
 
-#print(class_dict)
-print("Writing variant class...")
-vcf1 = 'hgmd_pro_2020.4_hg38_edited_vep-annotated.tsv'
-with open('hgmd_vep-annotated.tsv', "w") as out:
-    with open(vcf1, 'rt') as vcffp:
-        for cnt, line in enumerate(vcffp):
-            if not line.startswith("Chromosome"):
-                        line = line.rstrip("\n")
-                        cols = line.split("\t")
-                        var_info = cols[0]+"\t"+cols[1]+"\t"+cols[2]+"\t"+cols[3]
-                        new_line = line+"\t"+class_dict[var_info]
-                        out.write(new_line + "\n")
-                        #print(line+"\t"+class_dict[var_info])
-            else:
-                out.write(line.rstrip("\n") + "\thgmd_class\n")
+print(class_dict)
+#print("Writing variant class...")
+#vcf1 = 'merged_sig_norm_vep-annotated.tsv'
+#with open('merged_sig_norm_class_vep-annotated.tsv', "w") as out:
+#    with open(vcf1, 'rt') as vcffp:
+#        for cnt, line in enumerate(vcffp):
+#            if not line.startswith("Chromosome"):
+#                        line = line.rstrip("\n")
+#                        cols = line.split("\t")
+#                        var_info = cols[0]+"\t"+cols[1]+"\t"+cols[2]+"\t"+cols[3]
+#                        new_line = line+"\t"+class_dict[var_info]
+#                        out.write(new_line + "\n")
+#                        #print(line+"\t"+class_dict[var_info])
+#            else:
+#                out.write(line.rstrip("\n") + "\thgmd_class\n")
+#
--- src/training/data-prep/extract_variants.py
+++ src/training/data-prep/extract_variants.py
@@ -5,14 +5,14 @@ import yaml
 import re
 regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
 
-os.chdir( '/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/external/')
-vcf = 'merged.vcf.gz'
-output = '../interim/merged_sig.vcf'
+os.chdir( '/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/interim/')
+vcf = 'merged_norm.vcf.gz'
+output = 'merged_sig_norm.vcf'
 
 with open("../../configs/columns_config.yaml") as fh:
     config_dict = yaml.safe_load(fh)
 
-
+cln = hgmd = 0
 print("Collecting variant class...")
 with open(output, "w") as out:
     with gzip.open(vcf, 'rt') as vcffp:  #gzip.
@@ -25,12 +25,14 @@ with open(output, "w") as out:
                             if 'CLASS' in cols[7]:
                                 var_class = cols[7].split(";")[0].split("=")[1]
                                 if (var_class in config_dict['ClinicalSignificance']):
+                                    hgmd = hgmd+1
                                     new_line = var_info + "\t" + var_class
                                     out.write(new_line + "\n")
                             elif 'CLNSIG' in line:
                                 var_class = cols[7].split(";CLN")[5].split("=")[1]
                                 var_sub = cols[7].split(";CLN")[4].split("=")[1]
                                 if (var_class in config_dict['ClinicalSignificance']) and (var_sub in config_dict['CLNREVSTAT']):
+                                    cln=cln+1
                                     new_line = var_info + "\t" + var_class
                                     out.write(new_line + "\n")
                                     #class_dict[var_info] = var_class
@@ -43,21 +45,4 @@ with open(output, "w") as out:
             else:
                 out.write(line)
 
-
-
-
-
-#print("Writing variant class...")
-#vcf1 = 'hgmd_pro_2020.4_hg38_edited_vep-annotated.tsv'
-#with open('hgmd_vep-annotated.tsv', "w") as out:
-#    with open(vcf1, 'rt') as vcffp:
-#        for cnt, line in enumerate(vcffp):
-#            if not line.startswith("Chromosome"):
-#                        line = line.rstrip("\n")
-#                        cols = line.split("\t")
-#                        var_info = cols[0]+"\t"+cols[1]+"\t"+cols[2]+"\t"+cols[3]
-#                        new_line = line+"\t"+class_dict[var_info]
-#                        out.write(new_line + "\n")
-#                        #print(line+"\t"+class_dict[var_info])
-#            else:
-#                out.write(line.rstrip("\n") + "\thgmd_class\n")
+print(f'Clinvar variants: {cln}\nHGMD variants: {hgmd}\n')
