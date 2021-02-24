592c5072a93bfd6ebf8a11a55049ebda59fba807
Tarun Karthik Kumar Mamidi <tmamidi@uab.edu>
2021-02-24 14:50:49 -0600

    working with snv and non-snv datasets and training the models

--- src/training/data-prep/filter.py
+++ src/training/data-prep/filter.py
@@ -22,14 +22,25 @@ def get_col_configs(config_f):
 def extract_col(config_dict,df, stats):
     print('Extracting columns and rows according to config file !....')
     df = df[config_dict['columns']]
-    df= df.loc[df['hgmd_class'].isin(config_dict['ClinicalSignificance'])]
-    df=df[(df['Alternate Allele'].str.len() < 2)]
-    df=df[(df['Reference Allele'].str.len() < 2)]
+    #df = df[config_dict['Consequence']]
+    #df= df.loc[df['Consequence'].isin(config_dict['Consequence'])]
+    if 'non-snv' in stats:
+        df= df.loc[df['hgmd_class'].isin(config_dict['Clin_non_snv'])]
+        df=df[(df['Alternate Allele'].str.len() > 1) | (df['Reference Allele'].str.len() > 1)]
+        print('\nData shape (non-snv) =', df.shape, file=open(stats, "a"))
+        #print('Dropping empty columns and rows...')
+        #df.dropna(axis=1, thresh=(df.shape[1]*0.3), inplace=True)  #thresh=(df.shape[0]/4)
+        #df.dropna(axis=0, thresh=(df.shape[1]*0.3), inplace=True)  #thresh=(df.shape[1]*0.3),   how='all',
+        #df.dropna(axis=1, how='all', inplace=True)  #thresh=(df.shape[0]/4)
+    else:
+        df= df.loc[df['hgmd_class'].isin(config_dict['Clin_snv'])]
+        df=df[(df['Alternate Allele'].str.len() < 2) & (df['Reference Allele'].str.len() < 2)]
+        #df = df[df['BIOTYPE']=='protein_coding']
+        print('\nData shape (snv) =', df.shape, file=open(stats, "a"))
     print('Dropping empty columns and rows...')
-    #df.replace('.', np.nan, inplace=True)
-    #df[df.isnull().sum(axis=1)]
+    #df.dropna(axis=1, thresh=(df.shape[1]*0.3), inplace=True)  #thresh=(df.shape[0]/4)
+    df.dropna(axis=0, thresh=(df.shape[1]*0.5), inplace=True)  #thresh=(df.shape[1]*0.3),   how='all',
     df.dropna(axis=1, how='all', inplace=True)  #thresh=(df.shape[0]/4)
-    df.dropna(axis=0, thresh=(df.shape[1]*0.3), inplace=True)  #thresh=(df.shape[1]/1.2)    how='all',
     print('\nhgmd_class:\n', df['hgmd_class'].value_counts(), file=open(stats, "a"))
     print('\nclinvar_CLNSIG:\n', df['clinvar_CLNSIG'].value_counts(), file=open(stats, "a"))
     print('\nclinvar_CLNREVSTAT:\n', df['clinvar_CLNREVSTAT'].value_counts(), file=open(stats, "a"))
@@ -40,17 +51,18 @@ def extract_col(config_dict,df, stats):
     # CLNREVSTAT, CLNVC, MC
     return df
 
-def fill_na(df,config_dict): #(config_dict,df):
+def fill_na(df,config_dict, column_info): #(config_dict,df):
     #df1  = pd.DataFrame()
-    var = df[config_dict['snps']]
-    df = df.drop(config_dict['snps'], axis=1)
+    var = df[config_dict['var']]
+    df = df.drop(config_dict['var'], axis=1)
     #df.dtypes.to_csv('./data/processed/columns1.csv')
     #df.to_csv('./data/interim/temp.csv', index=False)
     #df = pd.read_csv('./data/interim/temp.csv')
     #os.remove('./data/interim/temp.csv')
     print('One-hot encoding...')
     df = pd.get_dummies(df, prefix_sep='_')
-    df.head(2).to_csv('./data/processed/merged_data_columns.csv', index=False)
+    print(df.columns.values.tolist(),file=open(column_info, "w"))
+    #df.head(2).to_csv(column_info, index=False)
     #lr = LinearRegression()
     #imp= IterativeImputer(estimator=lr, verbose=2, max_iter=10, tol=1e-10, imputation_order='roman')
     print('Filling NAs ....')
@@ -70,17 +82,16 @@ def fill_na(df,config_dict): #(config_dict,df):
     df = pd.concat([var.reset_index(drop=True), df], axis=1)
     return df
 
-def main(var_f, config_f, stats):
+def main(df, config_f, stats,column_info, null_info):
     # read QA config file
     config_dict = get_col_configs(config_f)
-    print('Config file loaded!\nNow loading data.....\n')
+    print('Config file loaded!')
     # read clinvar data
-    df = pd.read_csv(var_f, sep='\t')
-    print('Data Loaded !....')
+    
     print('\nData shape (Before filtering) =', df.shape, file=open(stats, "a"))
     df = extract_col(config_dict,df, stats)
     print('Columns extracted !....')
-    df.isnull().sum(axis = 0).to_csv('./data/processed/NA-counts-snp.csv')
+    df.isnull().sum(axis = 0).to_csv(null_info)
     #print('\n Unique Impact (Class):\n', df.hgmd_class.unique(), file=open("./data/processed/stats1.csv", "a"))
     y = df.hgmd_class.str.replace(r'DFP','high_impact').str.replace(r'DM\?','high_impact').str.replace(r'DM','high_impact')
     y = y.str.replace(r'Pathogenic/Likely_pathogenic','high_impact').str.replace(r'Likely_pathogenic','high_impact').str.replace(r'Pathogenic','high_impact')
@@ -89,18 +100,37 @@ def main(var_f, config_f, stats):
     print('\nImpact (Class):\n', y.value_counts(), file=open(stats, "a"))
     #y = df.hgmd_class
     df = df.drop('hgmd_class', axis=1)
-    df = fill_na(df,config_dict) #(config_dict,df)
+    df = fill_na(df,config_dict,column_info) #(config_dict,df)
     #print dataframe shape
     #df.dtypes.to_csv('../../data/interim/head.csv')
     print('\nData shape (After filtering) =', df.shape, file=open(stats, "a"))
     print('Class shape=', y.shape,file=open(stats, "a"))
-    df.to_csv('./data/processed/merged_data-snp.csv', index=False)
-    y.to_csv('./data/processed/merged_data-y-snp.csv', index=False)
-    return None
+    return df,y
 
 if __name__ == "__main__":
     os.chdir( '/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/')
-    var_f = "./data/processed/merged_sig_norm_class_vep-annotated.tsv"
+    print('Loading data...')
+    var_f = pd.read_csv("./data/processed/merged_sig_norm_class_vep-annotated.tsv", sep='\t')
+    print('Data Loaded !....')
     config_f = "./configs/columns_config.yaml"
-    stats = "./data/processed/stats-snp.csv"
-    main(var_f, config_f, stats)
+
+    #For non-snv variants
+    #stats = "./data/processed/stats-non-snv.csv"
+    #print("Filtering non-snv variants with at-least 50 percent data for each variant...", file=open(stats, "a"))
+    #print("Filtering non-snv variants with at-least 50 percent data for each variant...")
+    #column_info = './data/processed/non_snv_columns.csv'
+    #null_info = './data/processed/NA-counts-non-snv.csv'
+    #df,y = main(var_f, config_f, stats, column_info, null_info)
+    #df.to_csv('./data/processed/merged_data-non-snv.csv', index=False)
+    #y.to_csv('./data/processed/merged_data-y-non-snv.csv', index=False)
+    #del df,y
+
+    #For snv variants
+    stats = "./data/processed/stats-snv.csv"
+    print("Filtering snv variants with at-least 50 percent data for each variant...", file=open(stats, "a"))
+    print("Filtering snv variants with at-least 50 percent data for each variant...")
+    column_info = './data/processed/snv_columns.csv'
+    null_info = './data/processed/NA-counts-snv.csv'
+    df,y = main(var_f, config_f, stats, column_info, null_info)
+    df.to_csv('./data/processed/merged_data-snv.csv', index=False)
+    y.to_csv('./data/processed/merged_data-y-snv.csv', index=False)
--- src/training/training/ML_models.py
+++ src/training/training/ML_models.py
@@ -3,12 +3,14 @@ import numpy as np
 import pandas as pd
 import time
 import ray
+# Start Ray.
+ray.init(ignore_reinit_error=True)
 import pickle
 import shap
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 #from sklearn.feature_selection import RFE
 #from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split, cross_validate, cross_val_predict
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.preprocessing import label_binarize
 from sklearn.metrics import precision_score, roc_auc_score, accuracy_score, confusion_matrix, recall_score
 #from sklearn.multiclass import OneVsRestClassifier
@@ -21,59 +23,36 @@ import yaml
 import os
 os.chdir('/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
 
-# Start Ray.
-ray.init(ignore_reinit_error=True)
+@ray.remote
+def data_parsing(var,config_dict,output):
+    os.chdir('/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
+    data = 'merged_data-'+var+'.csv'
+    labels = 'merged_data-y-'+var+'.csv'
+    #print(f'{data}\t{labels}')
+    #Load data
+    print('\nUsing' +data+' ..', file=open(output, "a"))
+    X = pd.read_csv(data)
+    var = X[config_dict['ML_VAR']]
+    X = X.drop(config_dict['ML_VAR'], axis=1)
+    feature_names = X.columns.tolist()
+    X = X.values
+    y = pd.read_csv(labels)
+    print('Data Loaded!')
+    #Y = pd.get_dummies(y)
+    Y = label_binarize(y.values, classes=['low_impact', 'high_impact']) #'Benign', 'Likely_benign', 'Uncertain_significance', 'Likely_pathogenic', 'Pathogenic'
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.30, random_state=42)
+    del X,y
+    #scaler = StandardScaler().fit(X_train)
+    #X_train = scaler.transform(X_train)
+    #X_test = scaler.transform(X_test)
+    # explain all the predictions in the test set
+    background = shap.kmeans(X_train, 6)
+    return X_train, X_test, Y_train, Y_test, background, feature_names
 
-output = "/models/ML_results_snp.csv"
-
-with open("../../configs/columns_config.yaml") as fh:
-        config_dict = yaml.safe_load(fh)
-
-#Load data
-print('\nUsing merged_data-snp.csv..', file=open(output, "a"))
-X = pd.read_csv('merged_data-snp.csv')
-var = df[config_dict['snps']]
-X = X.drop(config_dict['snps'], axis=1)
-feature_names = X.columns.tolist()
-X = X.values
-# X[1]
-# var
-y = pd.read_csv('merged_data-y-snp.csv')
-#Y = pd.get_dummies(y)
-Y = label_binarize(y.values, classes=['low_impact', 'high_impact']) #'Benign', 'Likely_benign', 'Uncertain_significance', 'Likely_pathogenic', 'Pathogenic'
-
- 
-#scaler = StandardScaler().fit(X)
-#x_scaled = scaler.transform(X)
-#scaler = MinMaxScaler(feature_range=(-1,1))
-#x_scaled = scaler.fit_transform(X)
-#pd.DataFrame(x_scaled, columns= columns).to_csv('scaled_data.csv', index=False)
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.30, random_state=42)
-#scaler = StandardScaler().fit(X_train)
-#X_train = scaler.transform(X_train)
-#X_test = scaler.transform(X_test)
-
-# explain all the predictions in the test set
-background = shap.kmeans(X_train, 6)
-
-#Classifiers I wish to use
-classifiers = [
-	#KNeighborsClassifier(),
-	#SVC(class_weight='balanced', probability=True),
-    DecisionTreeClassifier(class_weight='balanced'),
-    LogisticRegression(class_weight='balanced'),
-    RandomForestClassifier(class_weight='balanced'),
-    AdaBoostClassifier(),
-    #GradientBoostingClassifier(),
-	#BaggingClassifier(),
-	ExtraTreesClassifier(class_weight='balanced_subsample'),
-    BalancedRandomForestClassifier(),
-    EasyEnsembleClassifier() # doctest: +SKIP
-]
 
 @ray.remote
 def classifier(clf, X_train, X_test, Y_train, Y_test,background,feature_names):
+   os.chdir('/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
    start = time.perf_counter()
    score = cross_validate(clf, X_train, Y_train, cv=10, return_train_score=True, return_estimator=True, n_jobs=-1, verbose=0)
    clf = score['estimator'][np.argmax(score['test_score'])]
@@ -100,17 +79,41 @@ def classifier(clf, X_train, X_test, Y_train, Y_test,background,feature_names):
    plt.figure()
    shap.summary_plot(shap_values, background, feature_names, show=False)
    #shap.plots.waterfall(shap_values[0], max_display=15)
-   plt.savefig("./models/"+clf_name + "_snp_features.pdf", format='pdf', dpi=1000, bbox_inches='tight')
+   plt.savefig("./models/"+clf_name + "_cons_features.pdf", format='pdf', dpi=1000, bbox_inches='tight')
 
    #list1 = [clf_name ,prc, roc_auc, accuracy, score, matrix, finish]
    list1 = [clf_name, np.mean(score['train_score']), np.mean(score['test_score']), prc, recall, roc_auc, accuracy, finish, matrix]
-   pickle.dump(clf, open("./models/"+clf_name+"_snp.pkl", 'wb'))
+   pickle.dump(clf, open("./models/"+clf_name+"_cons.pkl", 'wb'))
    return list1
 
-print('Model\tTrain_score(train_data)\tTest_score(train_data)\tPrecision(test_data)\tRecall\troc_auc\tAccuracy\tTime(min)\tConfusion_matrix[low_impact, high_impact]', file=open(output, "a"))    #\tConfusion_matrix[low_impact, high_impact]
-for i in classifiers:
-   list1 = ray.get(classifier.remote(i, X_train, X_test, Y_train, Y_test,background,feature_names))
-   print(f'{list1[0]}\t{list1[1]}\t{list1[2]}\t{list1[3]}\t{list1[4]}\t{list1[5]}\t{list1[6]}\t{list1[7]}\n{list1[8]}', file=open(output, "a"))
+if __name__ == "__main__":
+    #Classifiers I wish to use
+    classifiers = [
+        	#KNeighborsClassifier(),
+        	#SVC(class_weight='balanced', probability=True),
+            DecisionTreeClassifier(class_weight='balanced'),
+            LogisticRegression(class_weight='balanced'),
+            RandomForestClassifier(class_weight='balanced'),
+            AdaBoostClassifier(),
+            #GradientBoostingClassifier(),
+        	#BaggingClassifier(),
+        	ExtraTreesClassifier(class_weight='balanced_subsample'),
+            BalancedRandomForestClassifier(),
+            EasyEnsembleClassifier() # doctest: +SKIP
+        ]
+    output = "models/ML_results.csv"
+    print('Training datasets....', file=open(output, "a"))
+    with open("../../configs/columns_config.yaml") as fh:
+        config_dict = yaml.safe_load(fh)
 
+    variants = ['snv','non-snv','snv-protein-coding']
+    for var in variants:
+        print('Working with '+var+' dataset...', file=open(output, "a"))
+        print('Working with '+var+' dataset...')
+        X_train, X_test, Y_train, Y_test, background, feature_names = ray.get(data_parsing.remote(var,config_dict,output))
+        print('Model\tTrain_score(train_data)\tTest_score(train_data)\tPrecision(test_data)\tRecall\troc_auc\tAccuracy\tTime(min)\tConfusion_matrix[low_impact, high_impact]', file=open(output, "a"))    #\tConfusion_matrix[low_impact, high_impact]
+        for i in classifiers:
+           list1 = ray.get(classifier.remote(i, X_train, X_test, Y_train, Y_test,background,feature_names))
+           print(f'{list1[0]}\t{list1[1]}\t{list1[2]}\t{list1[3]}\t{list1[4]}\t{list1[5]}\t{list1[6]}\t{list1[7]}\n{list1[8]}', file=open(output, "a"))
+           print('done!')
 
-print('done!')
