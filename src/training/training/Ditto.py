6a6d220bb937c53b70d7f9c75fa6e11121addf3c
Tarun Karthik Kumar Mamidi <tmamidi@uab.edu>
2021-02-26 16:53:50 -0600

    running classifiers parallely

--- src/training/training/Ditto.py
+++ src/training/training/Ditto.py
@@ -1,60 +1,63 @@
-
-#https://towardsdatascience.com/how-to-use-gpus-for-machine-learning-with-the-new-nvidia-data-science-workstation-64ef37460fa0
-
 #from numpy import mean
 import numpy as np
-import cudf as pd
+import pandas as pd
 import time
 import ray
 # Start Ray.
 ray.init(ignore_reinit_error=True)
-import pickle
+from joblib import dump, load
 import shap
 #from sklearn.preprocessing import StandardScaler
 #from sklearn.feature_selection import RFE
 #from sklearn.preprocessing import MinMaxScaler
-import cuml
-from cuml.preprocessing.model_selection import train_test_split, cross_validate
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.preprocessing import label_binarize
 from sklearn.metrics import precision_score, roc_auc_score, accuracy_score, confusion_matrix, recall_score
 #from sklearn.multiclass import OneVsRestClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, BaggingClassifier, ExtraTreesClassifier
-from imblearn.ensemble import BalancedRandomForestClassifier, EasyEnsembleClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier
+from sklearn.naive_bayes import GaussianNB
+from imblearn.ensemble import BalancedRandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import matplotlib.pyplot as plt
 import yaml
 import os
 os.chdir('/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
 
-@ray.remote(num_cpus=3)
+@ray.remote
 def data_parsing(var,config_dict,output):
     os.chdir('/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
-    data = 'merged_data-'+var+'.csv'
-    labels = 'merged_data-y-'+var+'.csv'
-    #print(f'{data}\t{labels}')
     #Load data
-    print('\nUsing ' +data+' ..', file=open(output, "a"))
-    X = pd.read_csv(data)
-    var = X[config_dict['ML_VAR']]
-    X = X.drop(config_dict['ML_VAR'], axis=1)
-    feature_names = X.columns.tolist()
-    X = X.values
-    y = pd.read_csv(labels)
+    print(f'\nUsing merged_data-train_{var}..', file=open(output, "a"))
+    X_train = pd.read_csv(f'train_{var}/merged_data-train_{var}.csv')
+    #var = X_train[config_dict['ML_VAR']]
+    X_train = X_train.drop(config_dict['ML_VAR'], axis=1)
+    feature_names = X_train.columns.tolist()
+    X_train = X_train.values
+    Y_train = pd.read_csv(f'train_{var}/merged_data-y-train_{var}.csv')
+    Y_train = label_binarize(Y_train.values, classes=['low_impact', 'high_impact']) 
+
+    X_test = pd.read_csv(f'test_{var}/merged_data-test_{var}.csv')
+    #var = X_test[config_dict['ML_VAR']]
+    X_test = X_test.drop(config_dict['ML_VAR'], axis=1)
+    #feature_names = X_test.columns.tolist()
+    X_test = X_test.values
+    Y_test = pd.read_csv(f'test_{var}/merged_data-y-test_{var}.csv')
     print('Data Loaded!')
     #Y = pd.get_dummies(y)
-    Y = label_binarize(y.values, classes=['low_impact', 'high_impact']) #'Benign', 'Likely_benign', 'Uncertain_significance', 'Likely_pathogenic', 'Pathogenic'
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.30, random_state=42)
-    del X,y
+    Y_test = label_binarize(Y_test.values, classes=['low_impact', 'high_impact']) 
+    
     #scaler = StandardScaler().fit(X_train)
     #X_train = scaler.transform(X_train)
     #X_test = scaler.transform(X_test)
     # explain all the predictions in the test set
-    background = shap.kmeans(X_train, 6)
+    background = shap.kmeans(X_train, 10)
     return X_train, X_test, Y_train, Y_test, background, feature_names
 
 
-@ray.remote(num_gpus=3)
+@ray.remote(num_cpus=9)
 def classifier(clf,var, X_train, X_test, Y_train, Y_test,background,feature_names):
    os.chdir('/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
    start = time.perf_counter()
@@ -72,53 +75,55 @@ def classifier(clf,var, X_train, X_test, Y_train, Y_test,background,feature_name
    #score = clf.score(X_train, Y_train)
    #matrix = confusion_matrix(np.argmax(Y_test, axis=1), np.argmax(y_score, axis=1))
    matrix = confusion_matrix(Y_test, y_score,)
-   clf_name = str(type(clf)).split("'")[1].split(".")[3]
 
    # explain all the predictions in the test set
-   background = shap.kmeans(X_train, 6)
+   #background = shap.kmeans(X_train, 6)
    explainer = shap.KernelExplainer(clf.predict, background)
+   clf_name = str(type(clf)).split("'")[1].split(".")[3]
+   with open(f"./Ditto/{var}/{clf_name}_{var}.joblib", 'wb') as f:
+    dump(clf, f, compress='lz4')
+   del clf, X_train, Y_train
    background = X_test[np.random.choice(X_test.shape[0], 1000, replace=False)]
    shap_values = explainer.shap_values(background)
    plt.figure()
    shap.summary_plot(shap_values, background, feature_names, show=False)
    #shap.plots.waterfall(shap_values[0], max_display=15)
-   plt.savefig("./models/"+clf_name +"_"+ var+"_features.pdf", format='pdf', dpi=1000, bbox_inches='tight')
+   plt.savefig(f"./Ditto/{var}/{clf_name}_{var}_features.pdf", format='pdf', dpi=1000, bbox_inches='tight')
    finish = (time.perf_counter()-start)/60
    #list1 = [clf_name ,prc, roc_auc, accuracy, score, matrix, finish]
    list1 = [clf_name, np.mean(score['train_score']), np.mean(score['test_score']), prc, recall, roc_auc, accuracy, finish, matrix]
-   pickle.dump(clf, open("./models/"+clf_name+"_"+var+".pkl", 'wb'))
+   #pickle.dump(clf, open("./models/"+var+"/"+clf_name+"_"+var+".pkl", 'wb'))
    return list1
 
 if __name__ == "__main__":
     #Classifiers I wish to use
-    classifiers = [
-        	#KNeighborsClassifier(),
-        	#SVC(class_weight='balanced', probability=True),
-            DecisionTreeClassifier(class_weight='balanced'),
-            LogisticRegression(class_weight='balanced'),
-            RandomForestClassifier(class_weight='balanced'),
-            AdaBoostClassifier(),
-            #GradientBoostingClassifier(),
-        	#BaggingClassifier(),
-        	ExtraTreesClassifier(class_weight='balanced_subsample'),
-            BalancedRandomForestClassifier(),
-            EasyEnsembleClassifier() # doctest: +SKIP
-        ]
+    classifiers = VotingClassifier(estimators=[
+        ('DecisionTreeClassifier',DecisionTreeClassifier(class_weight='balanced')),
+        ('SGDClassifier',SGDClassifier(class_weight='balanced', n_jobs=-1)),
+        ('RandomForestClassifier',RandomForestClassifier(class_weight='balanced', n_jobs=-1)),
+        ('AdaBoostClassifier',AdaBoostClassifier()),
+	    ('ExtraTreesClassifier',ExtraTreesClassifier(class_weight='balanced', n_jobs=-1)),
+        ('BalancedRandomForestClassifier',BalancedRandomForestClassifier()),
+        ('GaussianNB',GaussianNB()),
+        ('LinearDiscriminantAnalysis',LinearDiscriminantAnalysis()),
+        ('MLPClassifier',MLPClassifier())
+        #,('EasyEnsembleClassifier',EasyEnsembleClassifier()) 
+        ], voting='hard', n_jobs=-1, verbose=1)
     
     
     with open("../../configs/columns_config.yaml") as fh:
         config_dict = yaml.safe_load(fh)
 
-    variants = ['snv','non-snv','snv-protein-coding']
+    variants = ['snv','non_snv','snv_protein_coding']
     for var in variants:
-        os.mkdir('models/'+var)
-        output = "models/"+var+"/ML_results_"+var+"_.csv"
-        print('Working with '+var+' dataset...', file=open(output, "a"))
+        if not os.path.exists('models/'+var):
+            os.makedirs('./Ditto/'+var)
+        output = "Ditto/"+var+"/ML_results_"+var+"_.csv"
+        print('Working with '+var+' dataset...', file=open(output, "w"))
         print('Working with '+var+' dataset...')
         X_train, X_test, Y_train, Y_test, background, feature_names = ray.get(data_parsing.remote(var,config_dict,output))
         print('Model\tTrain_score(train_data)\tTest_score(train_data)\tPrecision(test_data)\tRecall\troc_auc\tAccuracy\tTime(min)\tConfusion_matrix[low_impact, high_impact]', file=open(output, "a"))    #\tConfusion_matrix[low_impact, high_impact]
-        for i in classifiers:
-           list1 = ray.get(classifier.remote(i,var, X_train, X_test, Y_train, Y_test,background,feature_names))
-           print(f'{list1[0]}\t{list1[1]}\t{list1[2]}\t{list1[3]}\t{list1[4]}\t{list1[5]}\t{list1[6]}\t{list1[7]}\n{list1[8]}', file=open(output, "a"))
-           print('done!')
+        list1 = ray.get(classifier.remote(classifiers,var, X_train, X_test, Y_train, Y_test,background,feature_names))
+        print(f'{list1[0]}\t{list1[1]}\t{list1[2]}\t{list1[3]}\t{list1[4]}\t{list1[5]}\t{list1[6]}\t{list1[7]}\n{list1[8]}', file=open(output, "a"))
+        print('done!')
 
--- src/training/training/ML_models.py
+++ src/training/training/ML_models.py
@@ -5,7 +5,9 @@ import time
 import ray
 # Start Ray.
 ray.init(ignore_reinit_error=True)
-import pickle
+import warnings
+warnings.simplefilter("ignore")
+from joblib import dump, load
 import shap
 #from sklearn.preprocessing import StandardScaler
 #from sklearn.feature_selection import RFE
@@ -26,25 +28,25 @@ import yaml
 import os
 os.chdir('/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
 
-@ray.remote(num_cpus=3)
+@ray.remote
 def data_parsing(var,config_dict,output):
     os.chdir('/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
     #Load data
-    print('\nUsing merged_data-train_' +var+' ..', file=open(output, "a"))
-    X_train = pd.read_csv('train_snv/merged_data-train_snv.csv')
-    var = X_train[config_dict['ML_VAR']]
+    print(f'\nUsing merged_data-train_{var}..', file=open(output, "a"))
+    X_train = pd.read_csv(f'train_{var}/merged_data-train_{var}.csv')
+    #var = X_train[config_dict['ML_VAR']]
     X_train = X_train.drop(config_dict['ML_VAR'], axis=1)
     feature_names = X_train.columns.tolist()
     X_train = X_train.values
-    Y_train = pd.read_csv('train_snv/merged_data-y-train_snv.csv')
+    Y_train = pd.read_csv(f'train_{var}/merged_data-y-train_{var}.csv')
     Y_train = label_binarize(Y_train.values, classes=['low_impact', 'high_impact']) 
 
-    X_test = pd.read_csv('test_snv/merged_data-test_snv.csv')
-    var = X_test[config_dict['ML_VAR']]
+    X_test = pd.read_csv(f'test_{var}/merged_data-test_{var}.csv')
+    #var = X_test[config_dict['ML_VAR']]
     X_test = X_test.drop(config_dict['ML_VAR'], axis=1)
     #feature_names = X_test.columns.tolist()
     X_test = X_test.values
-    Y_test = pd.read_csv('test_snv/merged_data-y-test_snv.csv')
+    Y_test = pd.read_csv(f'test_{var}/merged_data-y-test_{var}.csv')
     print('Data Loaded!')
     #Y = pd.get_dummies(y)
     Y_test = label_binarize(Y_test.values, classes=['low_impact', 'high_impact']) 
@@ -57,15 +59,21 @@ def data_parsing(var,config_dict,output):
     return X_train, X_test, Y_train, Y_test, background, feature_names
 
 
-@ray.remote(num_cpus=3, num_gpus=3)
+@ray.remote(num_cpus=9)
 def classifier(clf,var, X_train, X_test, Y_train, Y_test,background,feature_names):
    os.chdir('/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/')
    start = time.perf_counter()
-   score = cross_validate(clf, X_train, Y_train, cv=10, return_train_score=True, return_estimator=True, n_jobs=-1, verbose=0)
-   clf = score['estimator'][np.argmax(score['test_score'])]
+   score = cross_validate(clf, X_train, Y_train, cv=10, return_train_score=True, return_estimator=True, n_jobs=-1, verbose=0, scoring=('roc_auc','neg_log_loss'))
+   clf = score['estimator'][np.argmin(score['test_neg_log_loss'])]
    #y_score = cross_val_predict(clf, X_train, Y_train, cv=5, n_jobs=-1, verbose=0)
    #class_weights = class_weight.compute_class_weight('balanced', np.unique(Y_train), Y_train)
    #clf.fit(X_train, Y_train) #, class_weight=class_weights)
+   clf_name = str(type(clf)).split("'")[1]  #.split(".")[3]
+   with open(f"./models/{var}/{clf_name}_{var}.joblib", 'wb') as f:
+    dump(clf, f, compress='lz4')
+   #del clf
+   #with open(f"./models/{var}/{clf_name}_{var}.joblib", 'rb') as f:
+   # clf = load(f)
    y_score = clf.predict(X_test)
    prc = precision_score(Y_test,y_score, average="weighted")
    recall  = recall_score(Y_test,y_score, average="weighted")
@@ -75,21 +83,21 @@ def classifier(clf,var, X_train, X_test, Y_train, Y_test,background,feature_name
    #score = clf.score(X_train, Y_train)
    #matrix = confusion_matrix(np.argmax(Y_test, axis=1), np.argmax(y_score, axis=1))
    matrix = confusion_matrix(Y_test, y_score,)
-   clf_name = str(type(clf)).split("'")[1].split(".")[3]
 
    # explain all the predictions in the test set
    #background = shap.kmeans(X_train, 6)
    explainer = shap.KernelExplainer(clf.predict, background)
+   del clf, X_train
    background = X_test[np.random.choice(X_test.shape[0], 1000, replace=False)]
    shap_values = explainer.shap_values(background)
    plt.figure()
    shap.summary_plot(shap_values, background, feature_names, show=False)
    #shap.plots.waterfall(shap_values[0], max_display=15)
-   plt.savefig("./models/"+var+"/"+clf_name +"_"+ var+"_features.pdf", format='pdf', dpi=1000, bbox_inches='tight')
+   plt.savefig(f"./models/{var}/{clf_name}_{var}_features.pdf", format='pdf', dpi=1000, bbox_inches='tight')
    finish = (time.perf_counter()-start)/60
    #list1 = [clf_name ,prc, roc_auc, accuracy, score, matrix, finish]
-   list1 = [clf_name, np.mean(score['train_score']), np.mean(score['test_score']), prc, recall, roc_auc, accuracy, finish, matrix]
-   pickle.dump(clf, open("./models/"+var+"/"+clf_name+"_"+var+".pkl", 'wb'))
+   list1 = [clf_name, np.mean(score['train_roc_auc']), np.mean(score['test_roc_auc']),np.mean(score['train_neg_log_loss']), np.mean(score['test_neg_log_loss']), prc, recall, roc_auc, accuracy, finish, matrix]
+   #pickle.dump(clf, open("./models/"+var+"/"+clf_name+"_"+var+".pkl", 'wb'))
    return list1
 
 if __name__ == "__main__":
@@ -114,14 +122,14 @@ if __name__ == "__main__":
     variants = ['snv','non_snv','snv_protein_coding']
     for var in variants:
         if not os.path.exists('models/'+var):
-            os.mkdir('models/'+var)
+            os.makedirs('./models/'+var)
         output = "models/"+var+"/ML_results_"+var+"_.csv"
         print('Working with '+var+' dataset...', file=open(output, "w"))
         print('Working with '+var+' dataset...')
         X_train, X_test, Y_train, Y_test, background, feature_names = ray.get(data_parsing.remote(var,config_dict,output))
-        print('Model\tTrain_score(train_data)\tTest_score(train_data)\tPrecision(test_data)\tRecall\troc_auc\tAccuracy\tTime(min)\tConfusion_matrix[low_impact, high_impact]', file=open(output, "a"))    #\tConfusion_matrix[low_impact, high_impact]
+        print('Model\tCross_validate(train_roc_auc)\tCross_validate(test_roc_auc)\tCross_validate(test_neg_log_loss)\tCross_validate(test_neg_log_loss)\tPrecision(test_data)\tRecall\troc_auc\tAccuracy\tTime(min)\tConfusion_matrix[low_impact, high_impact]', file=open(output, "a"))    #\tConfusion_matrix[low_impact, high_impact]
         for i in classifiers:
            list1 = ray.get(classifier.remote(i,var, X_train, X_test, Y_train, Y_test,background,feature_names))
-           print(f'{list1[0]}\t{list1[1]}\t{list1[2]}\t{list1[3]}\t{list1[4]}\t{list1[5]}\t{list1[6]}\t{list1[7]}\n{list1[8]}', file=open(output, "a"))
-           print('done!')
+           print(f'{list1[0]}\t{list1[1]}\t{list1[2]}\t{list1[3]}\t{list1[4]}\t{list1[5]}\t{list1[6]}\t{list1[7]}\t{list1[8]}\t{list1[9]}\n{list1[10]}', file=open(output, "a"))
+           print(f'{i} training and testing done!')
 
