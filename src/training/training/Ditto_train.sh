bfc0b5863f0077cba89d8187829fea381288f40e
Tarun Karthik Kumar Mamidi <tmamidi@uab.edu>
2021-06-22 11:47:37 -0500

    Ray tune seems to be working with hyperopt conditional search. Will update

--- src/training/training/Ditto_train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in {0..4} 
-do 
-    python slurm-launch.py --exp-name Ditto_tuning --command "python optuna-tpe-stacking_training.ipy --vtype snv_protein_coding" 
-    sleep 60 
-    rm Ditto_tuning*
-done
