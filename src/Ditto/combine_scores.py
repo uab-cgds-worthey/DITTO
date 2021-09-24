d7674da4a526a05bea25f195e8c850be62514740
Tarun Karthik Kumar Mamidi <tmamidi@uab.edu>
2021-09-24 14:11:55 -0500

    figured out the error. it was damn whitespace....smh

--- src/Ditto/combine_scores.py
+++ src/Ditto/combine_scores.py
@@ -41,11 +41,12 @@ if __name__ == "__main__":
             default="predictions_with_exomiser_100.csv",
             help="Output csv file with path for Top 100 variants")
         args = parser.parse_args()
+        print (args)
 
 
         ditto = pd.read_csv(args.ditto)
         raw = pd.read_csv(args.raw, sep='\t', usecols=['Chromosome','Position','Reference Allele','Alternate Allele','SYMBOL','Gene','Feature', 'HGNC_ID'])
-        #raw = raw[['Chromosome','Position','Reference Allele','Alternate Allele','SYMBOL','Gene','Feature', 'HGNC_ID']]		
+        #raw = raw[['Chromosome','Position','Reference Allele','Alternate Allele','SYMBOL','Gene','Feature', 'HGNC_ID']]
         print("Raw file loaded!")
 
         overall = pd.merge(raw, ditto, how='left', on=['Chromosome','Position','Alternate Allele','Reference Allele','Feature'])
@@ -61,7 +62,7 @@ if __name__ == "__main__":
             id_map = id_map.merge(exo_scores, left_on='NCBI gene ID', right_on='ENTREZ_GENE_ID')
             overall = overall.merge(id_map, how='left', left_on='HGNC_ID_x', right_on='HGNC ID')
             del id_map, exo_scores
-            overall = overall.sort_values(by = ['Ditto_Deleterious','EXOMISER_GENE_PHENO_SCORE'], axis=0, ascending=[False,False], kind='quicksort', ignore_index=True)
+            overall = overall.sort_values(by = ['EXOMISER_GENE_PHENO_SCORE','Ditto_Deleterious'], axis=0, ascending=[False,False], kind='quicksort', ignore_index=True)
             overall = overall[['Chromosome','Position','Reference Allele','Alternate Allele','EXOMISER_GENE_PHENO_SCORE', 'Ditto_Deleterious','SD','C']]
             overall.insert(0, 'PROBANDID', args.sample)
             overall.columns = ['PROBANDID','CHROM','POS','REF','ALT','E','P','SD','C']
@@ -75,7 +76,7 @@ if __name__ == "__main__":
             overall.columns = ['PROBANDID','CHROM','POS','REF','ALT','P','SD','C']
         overall = overall.reset_index(drop=True)
         overall.to_csv(args.output, index=False)
-        
+
         overall = overall.drop_duplicates(subset=['CHROM','POS','REF','ALT'], keep='first').reset_index(drop=True)
         overall.head(100).to_csv(args.output100, index=False, sep=':')
 
