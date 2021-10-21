265f3832f3db4933dddaeb1b3a137e5db945db8e
Manavalan Gajapathy <manag@uab.edu>
2021-10-21 11:47:17 -0500

    formats using black

--- src/slurm-launch.py
+++ src/slurm-launch.py
@@ -8,7 +8,7 @@ import sys
 import time
 import os
 
-#template_file = "slurm-template.sh" #Path(__file__) / 
+# template_file = "slurm-template.sh" #Path(__file__) /
 JOB_NAME = "${JOB_NAME}"
 NUM_NODES = "${NUM_NODES}"
 NUM_GPUS_PER_NODE = "${NUM_GPUS_PER_NODE}"
@@ -25,58 +25,57 @@ if __name__ == "__main__":
         "--exp-name",
         type=str,
         required=True,
-        help="The job name and path to logging file (exp_name.log).")
+        help="The job name and path to logging file (exp_name.log).",
+    )
     parser.add_argument(
         "--slurm-template",
         "-temp",
         type=str,
         default="./",
-        help="Path to slurm template. Default: ./ (current location)")
+        help="Path to slurm template. Default: ./ (current location)",
+    )
     parser.add_argument(
-        "--num-nodes",
-        "-n",
-        type=int,
-        default=1,
-        help="Number of nodes to use.")
+        "--num-nodes", "-n", type=int, default=1, help="Number of nodes to use."
+    )
     parser.add_argument(
         "--node",
         "-w",
         type=str,
         help="The specified nodes to use. Same format as the "
-        "return of 'sinfo'. Default: ''.")
+        "return of 'sinfo'. Default: ''.",
+    )
     parser.add_argument(
         "--num-gpus",
         type=int,
         default=0,
-        help="Number of GPUs to use in each node. (Default: 0)")
+        help="Number of GPUs to use in each node. (Default: 0)",
+    )
     parser.add_argument(
         "--num-cpus",
         type=int,
         default=10,
-        help="Number of CPUs to use in each node. (Default: 10)")
+        help="Number of CPUs to use in each node. (Default: 10)",
+    )
     parser.add_argument(
-        "--mem",
-        type=str,
-        default="150G",
-        help="Total Memory to use. (Default: 150G)")
+        "--mem", type=str, default="150G", help="Total Memory to use. (Default: 150G)"
+    )
     parser.add_argument(
-        "--partition",
-        type=str,
-        default="short",
-        help="Default partition: short"
+        "--partition", type=str, default="short", help="Default partition: short"
     )
     parser.add_argument(
         "--load-env",
         type=str,
         default="training",
-        help="Environment name to load before running script. (Default: 'training')")
+        help="Environment name to load before running script. (Default: 'training')",
+    )
     parser.add_argument(
         "--command",
         type=str,
         required=True,
         help="The command you wish to execute. For example: "
         " --command 'python ML_models.py'. "
-        "Note that the command must be a string.")
+        "Note that the command must be a string.",
+    )
     args = parser.parse_args()
 
     if args.node:
@@ -85,11 +84,13 @@ if __name__ == "__main__":
     else:
         node_info = ""
 
-    job_name = "{}_{}".format(args.exp_name,
-                              time.strftime("%m%d-%H%M%S", time.localtime()))
+    job_name = "{}_{}".format(
+        args.exp_name, time.strftime("%m%d-%H%M%S", time.localtime())
+    )
 
-    partition_option = "#SBATCH --partition={}".format(
-        args.partition) if args.partition else ""
+    partition_option = (
+        "#SBATCH --partition={}".format(args.partition) if args.partition else ""
+    )
 
     # ===== Modified the template script =====
     with open(f"{args.slurm_template}slurm-template.sh", "r") as f:
@@ -104,14 +105,14 @@ if __name__ == "__main__":
     text = text.replace(LOAD_ENV, str(args.load_env))
     text = text.replace(GIVEN_NODE, node_info)
     text = text.replace(
-        "# THIS FILE IS A TEMPLATE AND IT SHOULD NOT BE DEPLOYED TO "
-        "PRODUCTION!",
+        "# THIS FILE IS A TEMPLATE AND IT SHOULD NOT BE DEPLOYED TO " "PRODUCTION!",
         "# THIS FILE IS MODIFIED AUTOMATICALLY FROM TEMPLATE AND SHOULD BE "
-        "RUNNABLE!")
+        "RUNNABLE!",
+    )
 
     # ===== Save the script =====
     if not os.path.exists("./logs/"):
-            os.makedirs("./logs/")
+        os.makedirs("./logs/")
     script_file = "./logs/{}.sh".format(job_name)
     with open(script_file, "w") as f:
         f.write(text)
@@ -121,5 +122,7 @@ if __name__ == "__main__":
     subprocess.Popen(["sbatch", script_file])
     print(
         "Job submitted! Script file is at: <{}>. Log file is at: <{}>".format(
-            script_file, "./logs/{}.log".format(job_name)))
+            script_file, "./logs/{}.log".format(job_name)
+        )
+    )
     sys.exit(0)
