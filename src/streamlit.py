f976c798b406835a2baa9d42143cda3ba0c981b8
Tarun Mamidi <tmamidi@uab.edu>
2022-05-20 02:11:31 -0500

    working streamlit app

--- src/streamlit.py
+++ src/streamlit.py
@@ -26,16 +26,23 @@ def load_data():
     ) as f:
         clf = load(f)
 
+    X_train = pd.read_csv('./data/processed/train_custom_data-dbnsfp.csv.gz')
+    X_train = X_train.drop(['#chr','pos(1-based)','ref','alt','cds_strand','genename','Ensembl_transcriptid','clinvar_clnsig','Ensembl_geneid','Ensembl_proteinid','Uniprot_acc','clinvar_review','Interpro_domain'], axis=1)
+    X_train = X_train.values
+    background = shap.kmeans(X_train, 10)
+    explainer = shap.KernelExplainer(clf.predict_proba, background)
+    del clf,X_train,background
+
     image1 = Image.open("./data/processed/StackingClassifier_dbnsfp_features.jpg")
 
 
-    return pred, clf, image1
+    return pred, explainer, image1
 
 def main():
 
     col1, col2, col3 = st.columns(3)
 
-    pred, clf, image1 = load_data()
+    pred, explainer, image1 = load_data()
     col1.subheader("Variant details:")
     col3.subheader("DITTO overall feature importances")
     col3.image(image1, caption="DITTO overall feature importances generated using SHAP")
@@ -60,26 +67,24 @@ def main():
         ditto = pred[(pred.Gene==gene) & (pred.Transcript==trans) & (pred.position==pos) & (pred.ref_allele==ref) & (pred.alt_allele==alt)]
 
     row_idx = ditto.index.values[0]
-    col2.subheader(f"Ditto score = {ditto['Ditto'].values[0]}\n{row_idx}")
+    col2.subheader(f"Ditto score = {ditto['Ditto'].values[0]}")#\n{row_idx}")
 
-    with gzip.open("./data/processed/all_data_custom-dbnsfp.csv.gz", "rt") as vcffp:
-        for cnt, line in enumerate(vcffp):
-            if cnt == row_idx+1:
-                #col2.write(np.array(line.split(',')))
-                var_details = line.split(',')[:-106]
-                col2.write(line.split(',')[-106:])
-                col2.write(line.split(',')[:-106])
-                break
+    annots = pd.read_csv('./data/processed/all_data_custom-dbnsfp.csv.gz',skiprows=range(1,row_idx), nrows=1)
+    col2.write(f"Chromosome = {annots['#chr'].values[0]}")
+    col2.write(f"Pos(1-based) = {annots['pos(1-based)'].values[0]}")
+    col2.write(f"Reference allele = {annots['ref'].values[0]}")
+    col2.write(f"Alternate allele = {annots['alt'].values[0]}")
+    col2.write(f"Strand = {annots['cds_strand'].values[0]}")
+    col2.write(f"Gene name = {annots['genename'].values[0]}")
+    col2.write(f"Transcript = {annots['Ensembl_transcriptid'].values[0]}")
+    col2.write(f"Clinvar significance = {annots['clinvar_clnsig'].values[0]}")
+    annots = annots.drop(['#chr','pos(1-based)','ref','alt','cds_strand','genename','Ensembl_transcriptid','clinvar_clnsig','Ensembl_geneid','Ensembl_proteinid','Uniprot_acc','clinvar_review','Interpro_domain'], axis=1)
+    #col2.write(annots)
+    shap_values1 = explainer.shap_values(annots.iloc[0,:])
+    plt.figure()
+    shap.force_plot(explainer.expected_value[1], shap_values1[1], annots.iloc[0,:], matplotlib = True, show = True)
+    col2.pyplot(plt)
 
-    cols = ['Chromosome', 'position', 'ref_allele', 'alt_allele', 'strand', 'Gene', 'Transcript', 'Clinvar_significance', 'Clinvar_review', 'Clinvar_significance', 'Clinvar_significance']
-    #splits = re.split(':|>',option)
-    #col2.write(f"Chromosome: {splits[0]}")
-    #col2.write(f"position: {splits[1]}")
-    #col2.write(f"ref_allele: {splits[2]}")
-    #col2.write(f"alt_allele: {splits[3]}")
-    #col2.write(f"Gene: {splits[4]}")
-    #col2.write(f"Transcript: {splits[5]}")
-    #col2.write(f"Ditto deleterious: {var_list[option][1]}")
 
 if __name__ == "__main__":
     main()
