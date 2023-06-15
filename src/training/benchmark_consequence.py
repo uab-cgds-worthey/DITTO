453ed832e55f0862e194f25ddcf4963ae848db31
Tarun Karthik Kumar Mamidi <tmamidi@uab.edu>
2023-06-15 13:03:04 -0500

    fixing roc scores

--- src/training/benchmark_consequence.py
+++ src/training/benchmark_consequence.py
@@ -10,9 +10,10 @@ import matplotlib.pyplot as plt
 import functools
 print = functools.partial(print, flush=True)
 from sklearn.metrics import (
-    roc_curve, roc_auc_score, precision_recall_curve, average_precision_score
+    roc_curve, roc_auc_score, precision_recall_curve, average_precision_score, confusion_matrix, ConfusionMatrixDisplay, f1_score
 )
-from sklearn.preprocessing import label_binarize
+from sklearn.preprocessing import label_binarize, MinMaxScaler
+from sklearn.utils import class_weight
 import os
 from pathlib import Path
 import tensorflow as tf
@@ -20,7 +21,7 @@ from tensorflow import keras
 tf.config.run_functions_eagerly(True)
 
 
-def get_roc_plot(so, X_test, Y_test, outdir):
+def get_roc_plot(so, X_test, Y_test, outdir, weights):
 
     # prepare plots
     fig, [ax_roc, ax_prc] = plt.subplots(1, 2, figsize=(50, 20))
@@ -38,20 +39,23 @@ def get_roc_plot(so, X_test, Y_test, outdir):
 
     roc_scores_so = {}
     prc_scores_so = {}
+    f1_scores_so = {}
     for name in list(X_test.columns):
-            fpr, tpr, _ = roc_curve(Y_test, X_test[name].values)
-            auc = roc_auc_score(Y_test, X_test[name].values)
-
+            fpr, tpr, _ = roc_curve(Y_test, X_test[name].round(), sample_weight= weights)
+            auc = roc_auc_score(Y_test, X_test[name].round(), sample_weight= weights, average='weighted')
             auc = "{:.2f}".format(auc)
             roc_scores_so[name] = auc
             ax_roc.plot(fpr,tpr,label=str(name)+" = "+str(auc))
-            precision, recall, _ = precision_recall_curve(Y_test, X_test[name].values)
-            prc = average_precision_score(Y_test, X_test[name].values)
+            precision, recall, _ = precision_recall_curve(Y_test, X_test[name].round(), sample_weight= weights)
+            prc = average_precision_score(Y_test, X_test[name].round(), sample_weight= weights, average='weighted')
             prc = "{:.2f}".format(prc)
             prc_scores_so[name] = prc
+            f1 = f1_score(Y_test, X_test[name].round(), sample_weight= weights, average='weighted')
+            f1_scores_so[name] = "{:.2f}".format(f1)
             ax_prc.plot(recall,precision,label=str(name)+" = "+str(prc))
     ax_prc.legend( bbox_to_anchor=(1,0.5), loc="center left", fontsize=20)
     ax_roc.legend( bbox_to_anchor=(1,0.5), loc="center left", fontsize=20)
+    fig.tight_layout()
 
     fig.savefig(
         f"{outdir}/{so}_ROC_PRC_benchmarking.pdf",
@@ -59,15 +63,15 @@ def get_roc_plot(so, X_test, Y_test, outdir):
         dpi=1000,
         bbox_inches="tight",
     )
-    return roc_scores_so, prc_scores_so
+    return roc_scores_so, prc_scores_so, f1_scores_so
 
 def get_prediction(clf, X_test):
     y_score = clf.predict(X_test.values)
     return y_score
 
 def get_SHAP(test_x, clf, so, outdir, feature_names):
-    if test_x.shape[0] > 100:
-        sample_size = 100
+    if test_x.shape[0] > 500:
+        sample_size = 500
     else:
         sample_size = test_x.shape[0]
     background_x = test_x.sample(n=sample_size)
@@ -88,17 +92,24 @@ def get_SHAP(test_x, clf, so, outdir, feature_names):
         )
     return None
 
-def run_test(X_test, outdir, clf, config_dict, feature_names):
+def run_test(X_test, outdir, clf, config_dict, feature_names, class_weights):
     consequence_list = list(set(config_dict['consequence_cols']) & set(X_test.columns))
     roc_scores = {}
     prc_scores = {}
+    f1_scores = {}
     for so in consequence_list:
         roc_scores[so] = {}
         prc_scores[so] = {}
+        f1_scores[so] = {}
         test_x = X_test[X_test[so]==1]
         #try:
         benchmark_df = test_x[config_dict['Benchmark_cols'].keys()]
         benchmark_df.columns = benchmark_df.columns.to_series().map(config_dict['Benchmark_cols'])
+        benchmark_df_names = benchmark_df.columns.tolist()
+        min_max_scaler = MinMaxScaler()
+        benchmark_df = min_max_scaler.fit_transform(benchmark_df)
+        benchmark_df = pd.DataFrame(benchmark_df)
+        benchmark_df.columns = benchmark_df_names
         Y_test = test_x['class']
         test_x = test_x.drop(["class"], axis=1)
         if not test_x.empty and len(Y_test.unique())==2:
@@ -106,13 +117,15 @@ def run_test(X_test, outdir, clf, config_dict, feature_names):
             print(f"{so} class shape: {Y_test.value_counts()}")
             y_score = get_prediction(clf, test_x)
             benchmark_df['DITTO'] = y_score
-            roc_scores_so, prc_scores_so = get_roc_plot(so, benchmark_df, Y_test, outdir)
+            roc_scores_so, prc_scores_so, f1_scores_so = get_roc_plot(so, benchmark_df, Y_test, outdir, class_weights)
             roc_scores[so].update(roc_scores_so)
             prc_scores[so].update(prc_scores_so)
+            f1_scores[so].update(f1_scores_so)
             get_SHAP(test_x, clf, so, outdir, feature_names)
 
     pd.DataFrame(roc_scores).to_csv(f"{outdir}/NN_roc_scores.csv")
     pd.DataFrame(prc_scores).to_csv(f"{outdir}/NN_prc_scores.csv")
+    pd.DataFrame(f1_scores).to_csv(f"{outdir}/NN_f1_scores.csv")
     return None
 
 def data_parsing(test_x, config_dict):
@@ -125,13 +138,16 @@ def data_parsing(test_x, config_dict):
     X_test.fillna(X_test.mean(), inplace=True)
     feature_names = X_test.columns.tolist()
 
+    class_weights = class_weight.compute_class_weight(class_weight='balanced',classes=np.unique(Y_test),y=Y_test)
+    class_weights = {i:w for i,w in enumerate(class_weights)}
+    class_weights = np.array([class_weights[i] for i in  Y_test])
     Y_test = label_binarize(
     Y_test.values, classes=list(np.unique(Y_test))
         ).ravel().reshape(-1, 1)
     X_test['class'] = Y_test
     print("Data Loaded!")
 
-    return X_test, feature_names
+    return X_test, feature_names, class_weights
 
 def load_model(model_path):
     clf = keras.models.load_model(model_path + '/Neural_network')
@@ -199,8 +215,8 @@ if __name__ == "__main__":
     clf = load_model(
         ARGS.ditto
     )
-    X_test, feature_names = data_parsing(
+    X_test, feature_names, class_weights = data_parsing(
         ARGS.test_x, config_dict
     )
 
-    run_test(X_test, out_dir, clf, config_dict, feature_names)
+    run_test(X_test, out_dir, clf, config_dict, feature_names, class_weights)
