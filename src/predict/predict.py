c68ce4262b859dbc1c26132711369c21c6cd77b2
Tarun Mamidi <tmamidi@uab.edu>
2023-06-21 10:34:39 -0500

    predictions script

--- src/predict/predict.py
+++ src/predict/predict.py
@@ -1,112 +1,116 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#python src/Ditto/predict.py -i /data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/debugged/annotated_vcf/train/CAGI6_RGP_TRAIN_34_PROBAND/data.csv --sample CAGI6_RGP_TRAIN_34_PROBAND -o /data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/debugged/annotated_vcf/train/CAGI6_RGP_TRAIN_34_PROBAND/ditto_predictions.csv -o100 /data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/debugged/annotated_vcf/train/CAGI6_RGP_TRAIN_34_PROBAND/ditto_predictions_100.csv
+#python src/predict/predict.py -i /data/project/worthey_lab/projects/experimental_pipelines/tarun/DITTO/data/processed/lovd.parsed.csv.gz -o /data/project/worthey_lab/projects/experimental_pipelines/tarun/DITTO/data/processed/ditto_predictions.csv -c /data/project/worthey_lab/projects/experimental_pipelines/tarun/DITTO/configs/col_config.yaml -d /data/project/worthey_lab/projects/experimental_pipelines/tarun/DITTO/data/processed/train_data_3_star/
 
 import pandas as pd
 import yaml
-import warnings
-
-warnings.simplefilter("ignore")
-from joblib import load
 import argparse
 import os
-import glob
+from pathlib import Path
+from tensorflow import keras
+
+def test_parsing(dataframe, config_dict, clf):
+    # Drop variant info columns so we can perform one-hot encoding
+    # dataframe = dataframe[config_dict["raw_cols"]]
+    dataframe["so"] = dataframe["consequence"]
+    var = dataframe[config_dict["id_cols"]]
+    dataframe = dataframe.drop(config_dict["id_cols"], axis=1)
+    # dataframe = dataframe.replace(['.','-'], np.nan)
+
+    # Perform one-hot encoding
+    for key in config_dict["dummies_sep"]:
+        dataframe = pd.concat(
+            (dataframe, dataframe[key].str.get_dummies(sep=config_dict["dummies_sep"][key])), axis=1
+        )
+    dataframe = dataframe.drop(list(config_dict["dummies_sep"].keys()), axis=1)
+    dataframe = pd.get_dummies(dataframe, prefix_sep="_")
+
+    df2 = pd.DataFrame(columns=config_dict["filtered_cols"])
+    for key in config_dict["filtered_cols"]:
+        if key in dataframe.columns:
+            df2[key] = dataframe[key]
+        else:
+            df2[key] = 0
+    del dataframe
+
+    df2 = df2.drop(config_dict["train_cols"], axis=1)
+    for key in list(config_dict["median_scores"].keys()):
+        if key in df2.columns:
+            df2[key] = df2[key].fillna(config_dict["median_scores"][key]).astype("float64")
+
+    y_score = clf.predict(df2.values)
+    # print(df2.isnull().sum(axis=0))
+    y_score = pd.DataFrame(y_score, columns=["DITTO"])
+    # print(y_score)
+    # var["DITTO"] = y_score
+    var = pd.concat([var.reset_index(drop=True), y_score.reset_index(drop=True)], axis=1)
+    return var
+
+def is_valid_output_file(p, arg):
+    if os.access(Path(os.path.expandvars(arg)).parent, os.W_OK):
+        return os.path.expandvars(arg)
+    else:
+        p.error(f"Output file {arg} can't be accessed or is invalid!")
+
+
+def is_valid_file(p, arg):
+    if not Path(os.path.expandvars(arg)).is_file():
+        p.error(f"The file {arg} does not exist!")
+    else:
+        return os.path.expandvars(arg)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--input",
         "-i",
-        type=str,
         required=True,
         help="Input csv file with path for predictions",
-    )
-    parser.add_argument(
-        "--sample",
-        type=str,
-        # required=True,
-        help="Input sample name to showup in results",
+        type=lambda x: is_valid_file(parser, x),
+        metavar="\b",
     )
     parser.add_argument(
         "--output",
         "-o",
-        type=str,
         default="ditto_predictions.csv",
         help="Output csv file with path",
+        type=lambda x: is_valid_output_file(parser, x),
+        metavar="\b",
     )
     parser.add_argument(
-        "--output100",
-        "-o100",
-        type=str,
-        default="ditto_predictions_100.csv",
-        help="Output csv file with path for Top 100 variants",
+        "--config",
+        "-c",
+        required=True,
+        default="/data/project/worthey_lab/projects/experimental_pipelines/tarun/DITTO/configs/col_config.yaml",
+        help="config file to use for predictions",
+        type=lambda x: is_valid_file(parser, x),
+        metavar="\b",
     )
-    # parser.add_argument(
-    #    "--variant",
-    #    type=str,
-    #    help="Check index/rank of variant of interest. Format: chrX,101412604,C,T")
+    parser.add_argument(
+        "--DITTO",
+        "-d",
+        type=str,
+        #required=True,
+        default="/data/project/worthey_lab/projects/experimental_pipelines/tarun/DITTO/data/processed/train_data_3_star/",
+        help="DITTO model for predictions",
+        #type=lambda x: is_valid_file(parser, x),
+        #metavar="\b",
+    )
     args = parser.parse_args()
 
     # print("Loading data....")
 
     with open(
-        "/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/configs/testing.yaml"
+        args.config
     ) as fh:
         config_dict = yaml.safe_load(fh)
 
-    # with open('SL212589_genes.yaml') as fh:
-    #        config_dict = yaml.safe_load(fh)
+    X = pd.read_csv(args.input,low_memory=False)
 
-    X = pd.read_csv(args.input)
-    X_test = X
-    # print('Data Loaded!')
-    var = X_test[config_dict["ML_VAR"]]
-    X_test = X_test.drop(config_dict["ML_VAR"], axis=1)
-    X_test = X_test.values
+    clf = keras.models.load_model(f"{args.DITTO}/Neural_network")
+    clf.load_weights(f"{args.DITTO}/weights.h5")
 
-    with open(
-        "/data/project/worthey_lab/projects/experimental_pipelines/tarun/ditto/data/processed/models/F_3_0_1_nssnv/StackingClassifier_F_3_0_1_nssnv.joblib",
-        "rb",
-    ) as f:
-        clf = load(f)
-
-    # print('Ditto Loaded!\nRunning predictions.....')
-
-    y_score = clf.predict_proba(X_test)
-    del X_test
-    # print('Predictions finished!\nSorting ....')
-    pred = pd.DataFrame(y_score, columns=["Ditto_Benign", "Ditto_Deleterious"])
-
-    overall = pd.concat([var, pred], axis=1)
-
-    # overall = overall.merge(X,on='Gene')
-    del X, pred, y_score, clf
-    overall.drop_duplicates(inplace=True)
-    overall.insert(0, "PROBANDID", args.sample)
-    overall["SD"] = 0
-    overall["C"] = "*"
-    overall = overall.sort_values("Ditto_Deleterious", ascending=False)
-    # print('writing to database...')
-    overall.to_csv(args.output, index=False)
-    # print('Database storage complete!')
-
-    overall = overall.drop_duplicates(
-        subset=["Chromosome", "Position", "Alternate Allele", "Reference Allele"],
-        keep="first",
-    ).reset_index(drop=True)
-    overall = overall[
-        [
-            "PROBANDID",
-            "Chromosome",
-            "Position",
-            "Reference Allele",
-            "Alternate Allele",
-            "Ditto_Deleterious",
-            "SD",
-            "C",
-        ]
-    ]
-    overall.columns = ["PROBANDID", "CHROM", "POS", "REF", "ALT", "P", "SD", "C"]
-    overall.head(100).to_csv(args.output100, index=False, sep=":")
-    del overall
+    var = test_parsing(X, config_dict, clf)
+    var.to_csv(args.output, index=False)
